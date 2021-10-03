0f48d2296978e28673f7a30b37fe503c1b152d20
igordzie97 <34041060+igordzie97@users.noreply.github.com>
2021-10-04 00:16:08 +0200

    account test update / remove fixtures

--- cypress/integration/HealtCheck/Accounts.spec.ts
+++ cypress/integration/HealtCheck/Accounts.spec.ts
@@ -4,6 +4,10 @@ import AccountHelper from '@helpers/Accounts';
 describe('Test of Account endpoint', () => {
   const accountHelper = new AccountHelper();
 
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+
   it('should GET Bearer token with given username and password', () => {
     accountHelper.getToken(testAccount).then((token: string) => {
       cy.setLocalStorage('token', token);
@@ -12,16 +16,12 @@ describe('Test of Account endpoint', () => {
   });
 
   it('should GET information about account with given Bearer token', () => {
-    cy.restoreLocalStorage();
-
     cy.getLocalStorage('token').then((token) => {
       accountHelper.checkAccountAuth(token, testAccount.email);
     });
   });
 
   it('should properly GET Account baskets with given Bearer token', () => {
-    cy.restoreLocalStorage();
-
     cy.getLocalStorage('token').then((token) => {
       accountHelper.getAccountBasketsAuth(token);
     });
